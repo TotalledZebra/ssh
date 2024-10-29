80c600af5169bbc1de5a598bf600fba32874a7df
TotalledZebra <Holappa57@gmail.com>
2024-10-29 17:13:53 +0200

    User story 3

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,6 +1,5 @@
 
 class SpreadSheet:
-
     def __init__(self):
         self._cells = {}
 
@@ -9,11 +8,22 @@ class SpreadSheet:
 
     def evaluate(self, cell: str) -> int | str:
         value = self._cells.get(cell, '')
-        if value.startswith("'") and value.endswith("'"):
+        if value.startswith("="):
+            # Evaluate formula
+            if value.startswith("='") and value.endswith("'"):
+                return value[2:-1]
+            try:
+                # Evaluate as integer if possible
+                return int(value[1:])
+            except ValueError:
+                return "#Error"
+        elif value.startswith("'") and value.endswith("'"):
+            # Return string without quotes
             return value[1:-1]
         try:
-            int_value = int(value)
-            return int_value
+            # Try to convert directly to integer
+            return int(value)
         except ValueError:
+            # Not a valid integer
             return "#Error"
 
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -35,3 +35,27 @@ class TestSpreadSheet(TestCase):
         spreadsheet.set("A1", "Apple")
 
         self.assertEqual("#Error", spreadsheet.evaluate("A1"))
+
+    def test_evaluate_valid_string_formula(self):
+
+        spreadsheet = SpreadSheet()
+
+        spreadsheet.set("A1", "='Apple'")
+
+        self.assertEqual("Apple", spreadsheet.evaluate("A1"))
+
+    def test_evaluate_valid_integer_formula(self):
+
+        spreadsheet = SpreadSheet()
+
+        spreadsheet.set("A1", "=1")
+
+        self.assertEqual(1, spreadsheet.evaluate("A1"))
+
+    def test_evaluate_invalid_formula(self):
+
+        spreadsheet = SpreadSheet()
+
+        spreadsheet.set("A1", "='Apple")
+
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
