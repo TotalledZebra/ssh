66f357e5c6b34f238f643efcc470452dcd2f802c
TotalledZebra <Holappa57@gmail.com>
2024-10-29 17:06:40 +0200

    User story 1

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,14 +1,17 @@
+
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
 
-    def get(self, cell: str) -> str:
-        return self._cells.get(cell, '')
-
     def evaluate(self, cell: str) -> int | str:
-        pass
+        value = self._cells.get(cell, '')
+        try:
+            int_value = int(value)
+            return int_value
+        except ValueError:
+            return "#Error"
+
